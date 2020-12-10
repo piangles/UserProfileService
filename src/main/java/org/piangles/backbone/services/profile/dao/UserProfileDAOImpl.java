847ace9a88d163d8570de7fc2655190781c41923
saradhivs <saradhivs@yahoo.com>
2020-12-10 11:53:43 -0500

    Included PhoneNo.

--- src/main/java/org/piangles/backbone/services/profile/dao/UserProfileDAOImpl.java
+++ src/main/java/org/piangles/backbone/services/profile/dao/UserProfileDAOImpl.java
@@ -65,11 +65,12 @@ public class UserProfileDAOImpl extends AbstractDAO implements UserProfileDAO
 
 	public void updateUserProfile(String userId, BasicUserProfile profile) throws DAOException
 	{
-		super.executeSP(UPDATE_PROFILE_SP, 4, (stmt) -> {
+		super.executeSP(UPDATE_PROFILE_SP, 5, (stmt) -> {
 			stmt.setString(1, userId);
 			stmt.setString(2, profile.getFirstName());
 			stmt.setString(3, profile.getLastName());
 			stmt.setString(4, profile.getEMailId());
+			stmt.setString(5, profile.getPhoneNo());
 		});
 	}
 }
